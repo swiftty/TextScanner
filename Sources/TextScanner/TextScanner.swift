8d24380c3a5d9761060afb7773ae8faff003ba94
swiftty <62803132+swiftty@users.noreply.github.com>
2020-03-29 01:34:50 +0900

    add TextScanner

--- Sources/TextScanner/TextScanner.swift
+++ Sources/TextScanner/TextScanner.swift
@@ -1,3 +1,66 @@
-struct TextScanner {
-    var text = "Hello, World!"
+import struct Foundation.URL
+
+public struct TextScanner {
+    public struct Meta {
+        public let fileURL: URL?
+    }
+    public struct Location {
+        public private(set) var line = 0
+        public private(set) var column = 0
+        public private(set) var offset = 0
+
+        mutating func moveCaret(newLine isNewLine: Bool) {
+            if isNewLine {
+                line += 1
+                column = 0
+            } else {
+                column += 1
+            }
+            offset += 1
+        }
+    }
+
+    public var meta: Meta { storage.meta }
+    public var loc: Location { storage.loc }
+    public var isAtEnd: Bool { storage.isAtEnd }
+
+    public init(_ input: String) {
+        fatalError()
+    }
+
+    public init(fileURL: URL, encoding: String.Encoding = .utf8, chunkSize: Int = 4096) throws {
+        fatalError()
+    }
+
+    public func peek() -> Character? {
+        storage.peek()
+    }
+
+    @discardableResult
+    public mutating func next(where cond: (Character) -> Bool = { _ in true }) -> Character? {
+        copyStorageIfShared()
+        return storage.next(where: cond)
+    }
+
+    // MARK: - internal -
+
+    class Storage {
+        var meta: Meta { fatalError() }
+        var loc: Location { fatalError() }
+        var isAtEnd: Bool { fatalError() }
+
+        func peek() -> Character? { fatalError() }
+        func next(where cond: (Character) -> Bool = { _ in true }) -> Character? { fatalError() }
+
+        func copy() -> Self { fatalError() }
+    }
+
+    private(set) var storage: Storage
+
+    private mutating func copyStorageIfShared() {
+        if isKnownUniquelyReferenced(&storage) {
+            return
+        }
+        storage = storage.copy()
+    }
 }
